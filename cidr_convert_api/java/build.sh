224bf5cdb1427a17c4f62d4a5050a0d7fd241258
Samy Celis <33494707+ureshiichan@users.noreply.github.com>
2022-03-14 22:54:53 -0600

    cambios necesarios

--- cidr_convert_api/java/build.sh
+++ cidr_convert_api/java/build.sh
@@ -2,6 +2,11 @@
 
 set -ex
 
-cd cidr-api || exit 1
+
+cd cidr_convert_api || exit 1
+cd java
+cd cidr-api
+pwd
+ls
 mvn package
 java -cp target/cidr-api-1.0-SNAPSHOT-jar-with-dependencies.jar com.dott.App
