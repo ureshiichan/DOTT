82ed9b426e33d47c7b1276d2f7dcc632f516d754
Samy Celis <33494707+ureshiichan@users.noreply.github.com>
2022-03-17 23:53:14 -0600

    correccion de validIP

--- cidr_convert_api/java/cidr-api/src/main/java/com/dott/Convert.java
+++ cidr_convert_api/java/cidr-api/src/main/java/com/dott/Convert.java
@@ -11,6 +11,25 @@ public class Convert {
     }
 
     public static Boolean ipv4Validation(String x) {
-        return true;
+        String[] ipSplit = x.split("\\.");
+      
+       if(ipSplit.length!=4){
+            return false;
+        }
+        
+        boolean isValid=true;
+        
+        for(int i=0;i<ipSplit.length;i++){
+             int ipNum=-1;
+             try{
+                ipNum= Integer.parseInt(ipSplit[i]);
+            }catch(Exception e ){
+                isValid= false;
+            }
+            if(ipNum<0 || ipNum>255){
+                isValid = false;
+             }
+        }
+        return isValid;
     }
 }
